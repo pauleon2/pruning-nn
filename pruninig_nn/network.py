6d288f5ace96f7a3fe251875f00e1a89a36091f8
Paul Haeusner <haeusn01@gw.uni-passau.de>
2018-10-30 13:57:28 +0100

    Use Pruning wrapper for layers instead of direct use

--- pruninig_nn/network.py
+++ pruninig_nn/network.py
@@ -5,25 +5,28 @@ import torch.nn.functional as F
 
 class NeuralNetwork(nn.Module):
     """
-    Standard feed-forward neural network with one hidden layer
+    Standard feed-forward neural network with one hidden layer.
+    Currently activation function: ReLU
+    Output function: Log-Softmax
     """
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc1 = PruningLayer(nn.Linear(input_size, hidden_size))
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
+        self.fc2 = PruningLayer(nn.Linear(hidden_size, num_classes))
+        self.log_softmax = F.log_softmax
 
     def forward(self, x):
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
-        return F.log_softmax(out, dim=1)
+        return self.log_softmax(out, dim=1)
 
 
 class PruningLayer(nn.Module):
     """
-    Wrapper around modules from the nn package in pytorch.
-    Will add the capability to delete neurons and
+    Pruning Layer is a Decorator for all nn.Modules with a named parameter `weight`.
+    The parameter 'weight' will be changed by calling the prune method.
     """
     def __init__(self, wrapped):
         super().__init__()
@@ -51,7 +54,6 @@ class PruningLayer(nn.Module):
                 return param
 
     def forward(self, x):
-        for (name, param) in self.wrapped.named_parameters():
-            if name == 'weight':
-                param = param * self.mask
+        #  param = nn.Parameter(self.find_weight_in_params() * self.mask, True)
+        #  self.register_parameter('weight', param)
         return self.wrapped.forward(x)
